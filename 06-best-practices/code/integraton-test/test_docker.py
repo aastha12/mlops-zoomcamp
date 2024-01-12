2fcd5aacb37238d100623824d44f8075f8093dda
aasth <aasthajha123@gmail.com>
2024-01-11 23:32:29 -0800

    Added notes till module 6.3

--- 06-best-practices/code/integraton-test/test_docker.py
+++ 06-best-practices/code/integraton-test/test_docker.py
@@ -5,7 +5,7 @@ import json
 import requests
 from deepdiff import DeepDiff
 
-with open('event.json', 'rt', encoding='utf-8') as f_in:
+with open("/Users/aasth/Desktop/Data analytics/MLOps/datatalks-zoomcamp/mlops-zoomcamp/06-best-practices/code/integraton-test/event.json", 'rt', encoding='utf-8') as f_in:
     event = json.load(f_in)
 
 
